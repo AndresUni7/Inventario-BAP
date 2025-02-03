050973f2f2ba4f485e88a523bf1dc907d03920ae
AndresUni7 <brayan.pastran@uniminuto.edu.co>
2025-02-03 18:57:41 -0500

    Initial commit

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.5
+    </div>
+    <strong>Copyright &copy; 2024 <a href="#">UNI AN&DI</a>.</strong> Todos los derechos reservados
+  </footer>
